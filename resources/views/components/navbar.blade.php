6cba7ebb411f071d69f4ac4a0774b753cd190649
Adi Darmawan <adidrmwan17@gmail.com>
2020-01-13 09:04:38 +0700

    first commit

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,20 @@
+<nav id="colorlib-main-nav" class="border" role="navigation">
+  <a href="#" class="js-colorlib-nav-toggle colorlib-nav-toggle active"><i></i></a>
+  <div class="js-fullheight colorlib-table">
+    <div class="img" style="background-image: url(images/bg_2.jpg);"></div>
+    <div class="colorlib-table-cell js-fullheight">
+      <div class="row no-gutters">
+        <div class="col-md-12">
+          <h1 class="mb-4"><a href="index.html" class="logo">Garda Multi Solusi Teknika</a></h1>
+          <ul>
+            <li><a href="index.html"><span><small>01</small>Home</span></a></li>
+            <li><a href="about.html"><span><small>02</small>About Us</span></a></li>
+            <li><a href="services.html"><span><small>03</small>Services</span></a></li>
+            <li><a href="pricing.html"><span><small>04</small>Products</span></a></li>
+            <li><a href="contact.html"><span><small>05</small>Contact Us</span></a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
